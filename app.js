120e7827544bcec1d70b1da19a80ea4ebbc4ce82
soni <soni.rukmaji@gmail.com>
2017-09-05 21:21:29 +0530

    About Math

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-// variables are like containers, which hold values.
+var num1 = 10;
+var num2 = 2;
 
-var  firstName = "Amy";
-alert(firstName);
-console.log(firstName);
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2); // mod(%) returns remainder
 
-firstName = "David";  // changing firstName value to 'David'.
-console.log(firstName);
+var num = 11;
+console.log(num % num2);
+
+/* PEDMAS shows order of operations
+PEDMAS is an acronym for the words parenthesis, exponents, division, multiplication, addition, subtraction. */
+
+// following are some examples
+var num3 = 5;
+console.log((num1 + num2) * num3);
+console.log(num2 / num1 + num2 * (num3 - num2));
+
