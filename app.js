9e7f6894fcdd5ba0b9ff03c3411d9ae19338aa19
soni <soni.rukmaji@gmail.com>
2017-09-05 20:18:03 +0530

    About variables

---
+++ app.js
@@ -0,0 +1,8 @@
+// variables are like containers, which hold values.
+
+var  firstName = "Amy";
+alert(firstName);
+console.log(firstName);
+
+firstName = "David";  // changing firstName value to 'David'.
+console.log(firstName);
