43f36696a30f25c029f6d52b5eda27a077b98353
soni <soni.rukmaji@gmail.com>
2017-09-07 00:54:05 +0530

    Nested if - else statements

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var greeting = "what's your age?";
-var age = prompt(greeting);
+var greeting2 = "Do you have your driver's license?";
 
-if(age >= 21){
-	alert("you're buying the next round");
-}else{
-	alert("you're not quite old enough");
+var age = prompt(greeting);
+var license = prompt(greeting2);
+
+if(age >= 25){
+	if(license == 'yes')
+	{
+		alert("you qualify for insurance and it will be cheaper than if u were younger");
+	}else{
+		alert("you can't get insurance without license");
+	}
+}else if( age >= 18){
+	if(license == 'yes'){
+		alert("you qualify for insurance, but it will be expensive");
+	}else{
+		alert("you can't get insurance without license");
+	}
 }
