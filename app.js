19fef597a52d9f31a932628025f4408bacb72548
soni <soni.rukmaji@gmail.com>
2017-09-08 17:34:33 +0530

    About comments

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+/*
+multiple line comment
+Name :
+Algorithm: details about the algorithm
+Date Created: 
+*/
+
+//setting the variables
 var num1 = 10,
 	num2 = 2;
 
+//Ceating if statement to judge multiple input values
 if(num1 > num2 && num2 == 5){
 	alert('condition 1 passed');
 }
