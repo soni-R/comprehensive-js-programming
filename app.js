19d55028774ebfde0b8555573b73c89ff856f6d4
soni <soni.rukmaji@gmail.com>
2017-09-08 16:44:30 +0530

    nested if Else - better way

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ var greeting2 = "Do you have your driver's license?";
 var age = prompt(greeting);
 var license = prompt(greeting2);
 
-if(age >= 25){
-	if(license == 'yes')
-	{
-		alert("you qualify for insurance and it will be cheaper than if u were younger");
-	}else{
-		alert("you can't get insurance without license");
-	}
-}else if( age >= 18){
-	if(license == 'yes'){
+if(age >= 25 && license == 'yes'){
+	
+		alert("You qualify for insurance and it will be cheaper than if you were younger");
+
+}else if(age >= 18 && license == 'yes'){
+	
 		alert("you qualify for insurance, but it will be expensive");
-	}else{
+
+}else if(license == 'no'){
+
 		alert("you can't get insurance without license");
-	}
+}
+else{
+		alert("you can't get insurance");
 }
