168bf840167de207e8b5797d556522e3024577e8
soni <soni.rukmaji@gmail.com>
2017-09-08 18:16:00 +0530

    Switch Statement

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-/*
-multiple line comment
-Name :
-Algorithm: details about the algorithm
-Date Created: 
-*/
+var guess = prompt('Guess a number betwen 1 - 5');
 
-//setting the variables
-var num1 = 10,
-	num2 = 2;
+var num1 = '1';
+var num2 = '2';
+var num3 = '3';
+var num4 = '4';
+var num5 = '5';
 
-//Ceating if statement to judge multiple input values
-if(num1 > num2 && num2 == 5){
-	alert('condition 1 passed');
-}
-else if(num1 > num2 || num2 == 5){
-	alert('condition 2 passed');
+switch(guess){
+	case num1:
+	case num2:
+	case num3:
+	case num4:
+		alert("you missed the number");
+		break;
+
+	case num5:
+		alert("you guessed it!");
+		break;
+
+	default:
+		alert("you didn't give a number between 1-5");	
 }
