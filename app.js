f9ef0e07d6bf6c24b142d0e1504baa80ecf5e280
soni <soni.rukmaji@gmail.com>
2017-09-09 14:25:42 +0530

    While loop

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
-var guess = prompt('Guess a number betwen 1 - 5');
+var num = 1;
 
-var num1 = '1';
-var num2 = '2';
-var num3 = '3';
-var num4 = '4';
-var num5 = '5';
+//prints numbers 1 to 10
+while(num <= 10){
+	console.log(num);
+	num++; //increments number by 1, same as (num =  num+1), we can also increment by 2 or any number , num = num+2;
+} 
 
-switch(guess){
-	case num1:
-	case num2:
-	case num3:
-	case num4:
-		alert("you missed the number");
-		break;
-
-	case num5:
-		alert("you guessed it!");
-		break;
-
-	default:
-		alert("you didn't give a number between 1-5");	
+var num2 = 10
+while(num2 >= 0){
+	console.log(num2);
+	num2--; //prints 10 to 0
 }
