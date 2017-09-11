b42957f062c1ecd05a4e0f59af672224b36a697b
soni <soni.rukmaji@gmail.com>
2017-09-11 16:08:05 +0530

    Working with arrays

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var familyNames = new Array();
-familyNames[0] = 'Sam';
-familyNames[1] = 'John';
-familyNames[2] = 'David';
-
-// other ways to create arrays
-var flowers = ['rose', 'lilly', 'hibiscus'];  // This is array literal method
-
 var fruits = new Array("Apple", "Kiwi", "Grapes", "Banana");
+
+var myItem = fruits[3]; // getting the array value and storing it in variable  
+alert(myItem);  //displaying value using alert
+
+fruits[0] = 'Peach'; //changing value
+
+
+fruits = ["Mango", "Orange", "Strawberries"]; //this stmt completely overrides the array values 
+												// and display these values in console.
