7b1d751ed8ff37fa3ffb56a637f703afcd7ea490
soni <soni.rukmaji@gmail.com>
2017-09-11 15:41:15 +0530

    Creating arrays

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-for(var x = prompt("Guess a number between 1 and 50"); x != "n" && x != "N"; 
-	x = prompt("Guess a number between 1 and 50, type n if you want to quit the game"))
-{
-	if(x <= 24){
-		alert("Nope, Guess higher");
-	}else if(x >= 26){
-		alert("Nope, Guess lower");
-	}else{
-		alert("You Got It!!");
-		break;
-	}
-}
+var familyNames = new Array();
+familyNames[0] = 'Sam';
+familyNames[1] = 'John';
+familyNames[2] = 'David';
+
+// other ways to create arrays
+var flowers = ['rose', 'lilly', 'hibiscus'];  // This is array literal method
+
+var fruits = new Array("Apple", "Kiwi", "Grapes", "Banana");
